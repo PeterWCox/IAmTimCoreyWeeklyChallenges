e355cc93cdcdb90993f0d7f91c9cf807c7db2ed5
EurocodeHelpers <51972024+EurocodeHelpers@users.noreply.github.com>
2020-04-05 13:08:04 +0100

    Next commit.

---
+++ IATC Lectures/CallingAnApiDemo/CallingAnApiDemo_End/WPFApp/SunInfo.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for SunInfo.xaml
+    /// </summary>
+    public partial class SunInfo : Window
+    {
+        public SunInfo()
+        {
+            InitializeComponent();
+        }
+
+        private async void loadSunInfo_Click(object sender, RoutedEventArgs e)
+        {
+            var sunInfo = await SunProcessor.LoadSunInformation();
+
+            sunriseText.Text = $"Sunrise is at { sunInfo.Sunrise.ToLocalTime().ToShortTimeString() } ";
+            sunsetText.Text = $"Sunset is at { sunInfo.Sunset.ToLocalTime().ToShortTimeString() } ";
+        }
+    }
+}
